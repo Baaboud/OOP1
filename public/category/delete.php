fea58bc30bb4c839fbf5b3f87ee6ffda2d7d5f44
Baaboud <Baaboud.dev@gmail.com>
2022-03-19 12:01:31 +0300

    adding product & category classes with crud functions

--- public/category/delete.php
+++ public/category/delete.php
@@ -1,17 +1,8 @@
 <?php
-require_once('../connection.php');
+require_once("category.php");
 if(isset($_GET['DelID']))
          {
-             $gataId = $_GET['DelID'];
-             $query = " delete from category where id = '".$gataId."'"; // this query for delete record from table category
-             $result = mysqli_query($con,$query);
-             if($result)
-             {
-                 header("location:index.php");
-             }
-             else
-             {
-                 echo ' Please Check Your Query ';
-             }
+            $product = new Category();
+            $product->delete($_GET['DelID']);
         }
          ?>
